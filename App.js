086657493302183939f31d81dbafe238a6e93cc8
Victoria Kee <victoriakee95@gmail.com>
2021-12-15 13:32:48 -0600

    update app.js

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Main />
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0ff'
-  },
-});
