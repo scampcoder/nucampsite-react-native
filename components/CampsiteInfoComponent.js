dc872f55b7b99d08f4586342d095b3ddbd559a98
Victoria Kee <victoriakee95@gmail.com>
2021-12-16 10:35:49 -0600

    update imports in campsiteinfo

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
 function RenderCampsite({campsite}) {
     if (campsite) {
