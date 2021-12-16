fac98ecf4ac12860544bf71c27468f767f85703d
Victoria Kee <victoriakee95@gmail.com>
2021-12-16 09:53:13 -0600

    update imports in dir comp

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
 function Directory(props) {
 
