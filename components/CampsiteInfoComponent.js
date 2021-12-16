a306b259bc3943c3a073d27f11b3a84be3f53c50
Victoria Kee <victoriakee95@gmail.com>
2021-12-16 10:41:44 -0600

    fix errors, update campsite info to class comp

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -3,26 +3,41 @@ import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
 
-class RenderCampsite extends Component{
+function RenderCampsite({campsite}) {
+
+    if (campsite) {
+        return (
+            <Card
+                featuredTitle={campsite.name}
+                image={require('./images/react-lake.jpg')}
+            >
+                <Text style={{margin: 10}}>
+                    {campsite.description}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
+
+class CampsiteInfo extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES
-        }
+        };
     }
 
-    //sets title for screen
     static navigationOptions = {
         title: 'Campsite Information'
     }
 
     render() {
-        return <RenderCampsite campsite={this.props.campsite} />
+        const campsiteId = this.props.navigation.getParam('campsiteId');
+        const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
+        return <RenderCampsite campsite={campsite} />;
     }
-}
-
-function CampsiteInfo(props) {
-    return <RenderCampsite campsite={this.state.campsite} />;
 }
 
 export default CampsiteInfo;
