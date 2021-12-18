d966e150920a96e9bb6c6ed403e9289eaafa1938
Victoria Kee <victoriakee95@gmail.com>
2021-12-18 11:36:06 -0600

    set up mission comp

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import { ScrollView, Text } from "react-native";
 import { Card } from 'react-native-elements';
 
+function Mission() {
+    return (
+        <Card>
+            <Text style={{margin: 10}}>
+                We present a curated database of the best campsites 
+                in the vast woods and backcountry of the World Wide Web 
+                Wilderness. We increase access to adventure for the public 
+                while promoting safe and respectful use of resources. 
+                The expert wilderness trekkers on our staff personally verify 
+                each campsite to make sure that they are up to our standards. 
+                We also present a platform for campers to share reviews on campsites 
+                they have visited with each other.
+            </Text>
+        </Card>
+    )
+}
+
 class About extends Component {
     constructor(props) {
         super(props);
@@ -12,7 +29,11 @@ class About extends Component {
     }
 
     render() {
-        return <ScrollView></ScrollView>
+        return (
+            <ScrollView>
+                
+            </ScrollView>
+        )
     }
 }
 
