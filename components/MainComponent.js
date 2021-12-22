fd4622ce5599c294f46045e0fcea00dc312d3549
Victoria Kee <victoriakee95@gmail.com>
2021-12-22 00:42:41 -0600

    set nav options for directory

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -13,7 +13,17 @@ import Contact from './ContactComponent';
 
 const DirectoryNavigator = createStackNavigator(
     {
-        Directory: { screen: Directory },
+        Directory: { 
+            screen: Directory,
+            navigationOptions: ({navigation}) => ({
+                headerLeft: <Icon
+                        name='list'
+                        type='font-awesome'
+                        iconStyle={styles.stackIcon}
+                        onPress={() => navigation.toggleDrawer}
+                    />
+            })
+        },
         CampsiteInfo: { screen: CampsiteInfo }
     },
     {
