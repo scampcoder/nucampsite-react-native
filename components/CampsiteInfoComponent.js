94404dc94df3bebf91c45ca6b76e11765d90446e
Victoria Kee <victoriakee95@gmail.com>
2021-12-22 00:26:02 -0600

    add initial heart icon

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,73 +1,74 @@
-import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList } from 'react-native';
-import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
-import { COMMENTS } from '../shared/comments';
+import React, { Component } from "react";
+import { Text, View, ScrollView, FlatList } from "react-native";
+import { Card, Icon } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
+import { COMMENTS } from "../shared/comments";
 
-function RenderCampsite({campsite}) {
-
-    if (campsite) {
-        return (
-            <Card
-                featuredTitle={campsite.name}
-                image={require('./images/react-lake.jpg')}
-            >
-                <Text style={{margin: 10}}>
-                    {campsite.description}
-                </Text>
-            </Card>
-        );
-    }
-    return <View />;
+function RenderCampsite({ campsite }) {
+  if (campsite) {
+    return (
+      <Card
+        featuredTitle={campsite.name}
+        image={require("./images/react-lake.jpg")}
+      >
+        <Text style={{ margin: 10 }}>{campsite.description}</Text>
+        <Icon name="heart-o" type="font-awesome" color="#f50" raised reverse />
+      </Card>
+    );
+  }
+  return <View />;
 }
 
-function RenderComments({comments}) {
-
-    const renderCommentItem = ({item}) => {
-        return (
-            <View style={{margin: 10}}>
-                <Text style={{font: 14}}>{item.text}</Text>
-                <Text style={{font: 12}}>{item.rating} Stars</Text>
-                <Text style={{font: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
-            </View>
-        )
-    };
-    
+function RenderComments({ comments }) {
+  const renderCommentItem = ({ item }) => {
     return (
-        <Card title="Comments">
-            <FlatList 
-                data={comments} 
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()} />
-        </Card>
-    )
+      <View style={{ margin: 10 }}>
+        <Text style={{ font: 14 }}>{item.text}</Text>
+        <Text style={{ font: 12 }}>{item.rating} Stars</Text>
+        <Text style={{ font: 12 }}>{`-- ${item.author}, ${item.date}`}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <Card title="Comments">
+      <FlatList
+        data={comments}
+        renderItem={renderCommentItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </Card>
+  );
 }
 
 class CampsiteInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+      comments: COMMENTS,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS
-        };
-    }
+  static navigationOptions = {
+    title: "Campsite Information",
+  };
 
-    static navigationOptions = {
-        title: 'Campsite Information'
-    }
-
-    render() {
-        const campsiteId = this.props.navigation.getParam('campsiteId');
-        const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
-        const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId);
-        return (
-            <ScrollView>
-                <RenderCampsite campsite={campsite} />
-                <RenderComments comments={comments} />
-            </ScrollView>
-        );
-    }
+  render() {
+    const campsiteId = this.props.navigation.getParam("campsiteId");
+    const campsite = this.state.campsites.filter(
+      (campsite) => campsite.id === campsiteId
+    )[0];
+    const comments = this.state.comments.filter(
+      (comment) => comment.campsiteId === campsiteId
+    );
+    return (
+      <ScrollView>
+        <RenderCampsite campsite={campsite} />
+        <RenderComments comments={comments} />
+      </ScrollView>
+    );
+  }
 }
 
 export default CampsiteInfo;
