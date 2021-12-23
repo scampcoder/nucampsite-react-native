0633d575d77995e4fc27a058b98faf9c22b35fe0
Victoria Kee <victoriakee95@gmail.com>
2021-12-22 18:23:04 -0600

    set up stylesheet

--- components/LoadingComponent.js
+++ components/LoadingComponent.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+
+function Loading() {
+    return (
+
+    );
+}
+
+const styles = StyleSheet.create(
+    {
+        loadingView: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            flex: 1
+        },
+        loadingText: {
+            color: '#5637DD',
+            fontSize: 14,
+            fontWeight: 'bold'
+        }
+    }
+)
+
+export default Loading;
