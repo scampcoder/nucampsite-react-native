f56e556f2ff77153b33fb606b6245ff9f24bfb8d
Victoria Kee <victoriakee95@gmail.com>
2021-12-23 21:31:37 -0600

    set up imports for reservation component

---
+++ components/ReservationComponent.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { Text, View, ScrollView, StyleSheet,
+    Picker, Switch, Button } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+
