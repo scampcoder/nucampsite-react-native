0b7888a98b77b456012e198d14a52f8df148c657
Victoria Kee <victoriakee95@gmail.com>
2021-12-23 22:05:50 -0600

    set up stylesheet for reservation form

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -85,4 +85,21 @@ class Reservation extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+    formRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    formLabel: {
+        fontSize: 18,
+        flex: 2
+    },
+    formItem: {
+        flex: 1
+    }
+});
+
 export default Reservation;
