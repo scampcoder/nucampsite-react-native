8df080c9331119ed408017898eb07ded40d122ba
Victoria Kee <victoriakee95@gmail.com>
2021-12-28 14:55:13 -0600

    set up imports on favoritescomponent.js

---
+++ components/FavoritesComponent.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { FlatList, View, Text } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
