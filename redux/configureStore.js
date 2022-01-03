b9a45ce8d9ac2f5fda1e39fc84a5b1686aaf049a
Victoria Kee <victoriakee95@gmail.com>
2022-01-03 11:40:49 -0600

    update to persistCombineReducers using new config

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -17,7 +17,7 @@ const config = {
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             campsites,
             comments,
             partners,
