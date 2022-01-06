79a180f4d6e06626f21992d9f785b0c939c15286
Victoria Kee <victoriakee95@gmail.com>
2022-01-06 10:44:12 -0600

    fix typo

--- components/LoginComponenet.js
+++ components/LoginComponenet.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, Stylesheet } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
 import { Input, Checkbox } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 
