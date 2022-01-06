88d69b2cad3c010c7b2b91fb0e87f396781bdd25
Victoria Kee <victoriakee95@gmail.com>
2022-01-06 10:17:08 -0600

    set up login component with imports

---
+++ components/LoginComponenet.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Button, Stylesheet } from 'react-native';
+import { Input, Checkbox } from 'react-native-elements';
+import * as SecureStore from 'expo-secure-store';
+
+class Login extends Component {
+
+}
+
+export default Login;
