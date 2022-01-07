7360b4fc1c517a7986c1e7f206f0e4ee5cc838a9
Victoria Kee <victoriakee95@gmail.com>
2022-01-06 22:34:44 -0600

    create handle register method

--- components/LoginComponenet.js
+++ components/LoginComponenet.js
@@ -144,6 +144,19 @@ class RegisterTab extends Component {
         )
     }
 
+    handleRegister() {
+        console.log(JSON.stringify(this.state));
+        if(this.state.remember) {
+            SecureStore.setItemAsync('userinfo', JSON.stringify(
+                {username: this.state.username, password: this.state.password}))
+                //.setItemAsync is a promise so we need a catch for if there is an error
+                .catch(error => console.log('Could not save user info', error));
+        } else {
+            SecureStore.deleteItemAsync('userinfo')
+                .catch(error => console.log('Could not delete user info', error));
+        }
+    }
+
     render() {
         return (
             <ScrollView>
